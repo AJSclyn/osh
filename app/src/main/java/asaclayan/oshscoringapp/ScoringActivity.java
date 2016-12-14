30733e4824fffc891f9493f60569f0d6828b6ad5
AJ Saclayan <ajsaclayan@gmail.com>
2016-12-13 20:03:29 -0500

    overwrite empty project

---
+++ app/src/main/java/asaclayan/oshscoringapp/ScoringActivity.java
@@ -0,0 +1,13 @@
+package asaclayan.oshscoringapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoring);
+    }
+}
